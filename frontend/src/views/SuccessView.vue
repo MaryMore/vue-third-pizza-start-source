ca731d9d136ba976c1ee86c6a50c2ee388f21204
Mary <m.zhivilo@gmail.com>
2025-02-15 00:27:11 +0700

    Выполнены задания раздела 6

--- frontend/src/views/SuccessView.vue
+++ frontend/src/views/SuccessView.vue
@@ -1,4 +1,5 @@
 <template>
+  <transition name="fade" appear>
     <app-popup>
       <router-link :to="{ name: 'orders' }" class="close">
         <span class="visually-hidden">Закрыть поп-ап</span>
@@ -11,20 +12,19 @@
         </router-link>
       </app-popup-button>
     </app-popup>
-  </template>
-  
-  <script setup>
-  import AppPopup from "@/common/components/popup/AppPopup.vue";
-  import AppPopupTitle from "@/common/components/popup/AppPopupTitle.vue";
-  import AppPopupButton from "@/common/components/popup/AppPopupButton.vue";
-  import { computed } from "vue";
-  import { useAuthStore } from "@/stores/auth";
-  
-  const authStore = useAuthStore();
-  
-  const redirectRouteName = computed(() =>
-    authStore.isAuthenticated ? "orders" : "home"
-  );
-  </script>
-  
-  
+  </transition>
+</template>
+
+<script setup>
+import AppPopup from "@/common/components/popup/AppPopup.vue";
+import AppPopupTitle from "@/common/components/popup/AppPopupTitle.vue";
+import AppPopupButton from "@/common/components/popup/AppPopupButton.vue";
+import { computed } from "vue";
+import { useAuthStore } from "@/stores/auth";
+
+const authStore = useAuthStore();
+
+const redirectRouteName = computed(() =>
+  authStore.isAuthenticated ? "orders" : "home",
+);
+</script>
